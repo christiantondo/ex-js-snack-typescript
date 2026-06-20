b08ebcc983aa9bcf3bea16d3758f1fc49e9a7fc7
Christian Tondo <christiantondo1@gmail.com>
2026-06-20 18:58:03 +0200

    snack 2 completed

--- src/main.ts
+++ src/main.ts
@@ -30,12 +30,19 @@ if (typeof data === "string") {
 
 // 🏆 Snack 2
 // Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
-
 // nome → stringa
 // cognome → stringa
 // annoNascita → numero
 // sesso → Può essere solo "m" o "f".
 // anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
+type Dipendente = {
+    nome: string,
+    cognome: string,
+    annoNascita: number,
+    sesso: "m" | "f",
+    anniDiServizio: number[]
+}
 
 // 🎯 BONUS
 // Il type alias Dipendente, ha anche i seguenti dati:
