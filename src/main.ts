632453d630a0e3732d2b1fd54c3bb32f4175aef2
Christian Tondo <christiantondo1@gmail.com>
2026-06-20 19:10:58 +0200

    snack 3 completed

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,8 @@ if (typeof data === "string") {
     data.then(res => console.log(res))
 } else {
     console.log("“Tipo non supportato”");
-}
+};
+
 // 🎯 BONUS
 // Se è null: stampa “Il dato è vuoto”
 // Se è un array: stampa la sua lunghezza
@@ -42,7 +43,7 @@ type Dipendente = {
     annoNascita: number,
     sesso: "m" | "f",
     anniDiServizio: number[]
-}
+};
 
 // 🎯 BONUS
 // Il type alias Dipendente, ha anche i seguenti dati:
@@ -52,15 +53,27 @@ type Dipendente = {
 
 // 🏆 Snack 3
 // Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
-
 // Developer
 // livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
 // linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
 // certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+
 // ProjectManager
 // teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
 // budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
 // stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+
+type Developer = Dipendente & {
+    livelloEsperienza: "Junior" | "Mid" | "Senior",
+    linguaggi?: string[],
+    certificazioni: string[]
+};
+
+type ProjectManager = Dipendente & {
+    teamSize: number | null,
+    budgetGestito?: number,
+    stakeholderPrincipali: string[]
+}
 
 // 🎯 BONUS
 // Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
