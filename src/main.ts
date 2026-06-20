49e3b5e2847d696b51d4ecd9951036131b073fc4
Christian Tondo <christiantondo1@gmail.com>
2026-06-20 18:45:30 +0200

    snack 1 completed

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@
 // Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
 
 // Se è una stringa: stampala in maiuscolo
-
 // Se è un numero: moltiplicalo per due e stampalo
-
 // Se è un booleano: stampa “Sì” o “No” in base al suo valore
-
 // In tutti gli altri casi: stampa “Tipo non supportato”
+
+let data: unknown;
+if (typeof data === "string") {
+    console.log(data.toUpperCase());
+} else if (typeof data === "number") {
+    console.log(data * 2);
+} else if (typeof data === "boolean") {
+    console.log(data ? "Si" : "No");
+} else {
+    console.log("“Tipo non supportato”");
+}
 
 // 🎯 BONUS
 // Se è null: stampa “Il dato è vuoto”
